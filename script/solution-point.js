d636875aab4a2af7efbb26aa80f846ce0275fe16
Ather1230 <ather.biswas0057@gmail.com>
2022-09-11 19:33:27 +0600

    updated

--- script/solution-point.js
+++ script/solution-point.js
@@ -2,3 +2,8 @@
 // 2.not add product to cart => solve
 //3. product price fix => solve
 //4.product total price fix => solve
+// 5.Product title not found => solve
+//6. product id not found => solve
+//7. product show less than have product => solve
+//8.Delivery charge change => solve
+//9.search button do not work => solve
